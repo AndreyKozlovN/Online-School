b07bc1861ac25fcf052ddd25081f1cee16f9aa96
AndreyKozlovN <kozlov205an@gmail.com>
2022-12-20 16:19:13 +0400

    before create repo

--- components/P/P.props.ts
+++ components/P/P.props.ts
@@ -1,6 +1,10 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
 
-export interface PProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {
-	size?: "big" | "normal" | "little";
-	children: ReactNode;
+export interface PProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLParagraphElement>,
+    HTMLParagraphElement
+  > {
+  size?: "big" | "normal" | "little";
+  children: ReactNode;
 }
--- components/P/P.tsx
+++ components/P/P.tsx
@@ -1,17 +1,23 @@
-import styles from './P.module.css';
+import styles from "./P.module.css";
 import { PProps } from "./P.props";
 import cn from "classnames";
 
-export const P = ({ size = "normal", children, className, ...props }: PProps): JSX.Element => {
-	return (
-		<p
-			className={cn(styles.p, className, {
-				[styles.big]: size == "big",
-				[styles.normal]: size == "normal",
-				[styles.little]: size == "little"
-			})}{...props}
-		>
-			{children}
-		</p>
-	);
+export const P = ({
+  size = "normal",
+  children,
+  className,
+  ...props
+}: PProps): JSX.Element => {
+  return (
+    <p
+      className={cn(styles.p, className, {
+        [styles.big]: size == "big",
+        [styles.normal]: size == "normal",
+        [styles.little]: size == "little",
+      })}
+      {...props}
+    >
+      {children}
+    </p>
+  );
 };
