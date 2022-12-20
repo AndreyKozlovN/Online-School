dcc525e9be6164929dd6f97318d919c87d9551c8
AndreyKozlovN <kozlov205an@gmail.com>
2022-12-20 17:04:28 +0400

    Добавлен контекст для передачи данных по приложению

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetStaticProps } from "next";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 import { Button, Htag, P, Tag, Rating } from "../components";
 import { withLayout } from "../layout/Layout";
@@ -10,20 +10,27 @@ function Home({ menu }: HomeProps): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance="primary" arrow="right">Кнопка</Button>
-      <Button appearance="ghost" arrow="down">Кнопка</Button>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Кнопка
+      </Button>
       <P size="little">Маленький текст</P>
       <P>Средний текст</P>
       <P size="big">Большой текст</P>
-      <Tag color="red" href="https://google.com">Google</Tag>
+      <Tag color="red" href="https://google.com">
+        Google
+      </Tag>
       <Tag color="green">Тэг</Tag>
-      <Tag size="little" color="primary">Маленький</Tag>
-      <Tag color="gray" size="little">Маленький</Tag>
+      <Tag size="little" color="primary">
+        Маленький
+      </Tag>
+      <Tag color="gray" size="little">
+        Маленький
+      </Tag>
       <Tag>Тэг</Tag>
       <Rating rating={rating} isEditable={true} setRating={setRating} />
-      <ul>
-        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-      </ul>
     </>
   );
 }
@@ -32,14 +39,17 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {
-    firstCategory
-  });
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
   return {
     props: {
       menu,
-      firstCategory
-    }
+      firstCategory,
+    },
   };
 };
 
