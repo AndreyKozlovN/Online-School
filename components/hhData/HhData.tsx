742d7986a2784f11f7fdc93277247c6d3271b52d
AndreyKozlovN <kozlov205an@gmail.com>
2023-05-15 12:46:09 +0400

    написан regex для красивого отображения стоимости

--- components/hhData/HhData.tsx
+++ components/hhData/HhData.tsx
@@ -1,3 +1,4 @@
+import { priceRu } from "../../helpers/helpers";
 import { Card } from "../Card/Card";
 import styles from "./HhData.module.css";
 import { HhDataProps } from "./HhData.props";
@@ -20,7 +21,7 @@ export const HhData = ({
       <Card className={styles.salary}>
         <div>
           <div className={styles.title}>Начальный</div>
-          <div className={styles.salaryValue}>{juniorSalary}</div>
+          <div className={styles.salaryValue}>{priceRu(juniorSalary)}</div>
           <div className={styles.rate}>
             <RateIcon className={styles.filled} />
             <RateIcon />
@@ -30,7 +31,7 @@ export const HhData = ({
 
         <div>
           <div className={styles.title}>Средний</div>
-          <div className={styles.salaryValue}>{middleSalary}</div>
+          <div className={styles.salaryValue}>{priceRu(middleSalary)}</div>
           <div className={styles.rate}>
             <RateIcon className={styles.filled} />
             <RateIcon className={styles.filled} />
@@ -40,7 +41,7 @@ export const HhData = ({
 
         <div>
           <div className={styles.title}>Профессионал</div>
-          <div className={styles.salaryValue}>{seniorSalary}</div>
+          <div className={styles.salaryValue}>{priceRu(seniorSalary)}</div>
           <div className={styles.rate}>
             <RateIcon className={styles.filled} />
             <RateIcon className={styles.filled} />
